c62a8f1afc026236958860260ddc8acbea8ad786
Matej Kukolik <dalinec123@gmail.com>
2022-08-23 14:45:39 +0200

    add sign out functionality

--- src/components/sing-up-form/sign-up-form.component.jsx
+++ src/components/sing-up-form/sign-up-form.component.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
-import "./sing-up-form.styles.scss";
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../contexts/user.context";
+
+import "./sing-up-form.styles.scss";
 
 const defaultFormFields = {
   displayName: "",
@@ -21,7 +24,10 @@ const SingUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
+  const {setCurrentUser} = useContext(UserContext)
+
+
+  console.log("hit");
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -39,6 +45,8 @@ const SingUpForm = () => {
         email,
         password
       );
+
+      setCurrentUser(user)
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
