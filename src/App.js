ed402eeb041e6d702ac5be2c981009c277db0647
Matej Kukolik <dalinec123@gmail.com>
2022-08-24 11:31:10 +0200

    add products context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Navigation from "./components/routes/navigation/navigation.component";
-import Home from "./components/routes/home/home.component";
-import Authentication from "./components/routes/authentication/authentication.component";
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Navigation from './components/routes/navigation/navigation.component';
+import Home from './components/routes/home/home.component';
+import Authentication from './components/routes/authentication/authentication.component';
+import Shop from './components/routes/shop/shop.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}></Route>
-        <Route path="shop" element={<Shop />}></Route>
-        <Route path="auth" element={<Authentication />}></Route>
+        <Route path='shop' element={<Shop />}></Route>
+        <Route path='auth' element={<Authentication />}></Route>
       </Route>
     </Routes>
   );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./App";
-import { UserProvider } from "./contexts/user.context";
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
-import "./index.scss";
-import reportWebVitals from "./reportWebVitals";
+import './index.scss';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
