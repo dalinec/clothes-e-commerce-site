e4d2384ed66d937b394dab869ccd3528012675c4
Matej Kukolik <dalinec123@gmail.com>
2022-08-25 10:57:51 +0200

    add Cart item design + counter functionality

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,12 +1,17 @@
 import './cart-item.styles.scss';
 
 const CartItem = ({ cartItem }) => {
-  const { name, qty } = cartItem;
+  const { name, qty, price, imageUrl } = cartItem;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{qty}</span>
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {qty} x ${price}
+        </span>
+      </div>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -21,17 +21,34 @@ export const CartContext = createContext({
   setIsOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.qty,
+      0
+    );
+
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (product) => {
     setCartItems(addCartItem(cartItems, product));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
