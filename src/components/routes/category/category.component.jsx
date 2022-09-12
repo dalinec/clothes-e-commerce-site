8f1191af7133ed5f990a4acc8d0238be24fe2dc0
Matej Kukolik <dalinec123@gmail.com>
2022-09-12 14:10:19 +0200

    add redux-thunk and a loading spinner

--- src/components/routes/category/category.component.jsx
+++ src/components/routes/category/category.component.jsx
@@ -3,12 +3,19 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../product-card/product-card.component';
-import { selectCategoriesMap } from '../../../store/categories/category.selector';
+import Spinner from '../../spinner/spinner.component';
+
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from '../../../store/categories/category.selector';
+
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -18,12 +25,16 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products && // the products && acts like a safeguard, you only render the components if the actual data is present
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {isLoading ? ( //if isLoading is true, render spinner otherwise render categories
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products && // the products && acts like a safeguard, you only render the components if the actual data is present
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -2,36 +2,33 @@ import { legacy_createStore as createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-};
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+//if it evaluates false, it returns an empty array
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancer =
+  process.env.NODE_ENV !== 'production' &&
+  window &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const composedEnhancers =
+  composedEnhancer(applyMiddleware(...middleWares)) || compose;
 
 export const store = createStore(
   persistedReducer,
