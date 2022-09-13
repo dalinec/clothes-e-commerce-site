26af9245477adecf0f5a681d28060cb78bd0c451
Matej Kukolik <dalinec123@gmail.com>
2022-09-13 14:47:01 +0200

    add sign-out in sagas

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,16 +1,15 @@
 import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CartIcon from '../../cart-icon/cart-icon.component';
 import CartDropdown from '../../cart-dropdown/cart-dropdown.component';
 
 import { selectCurrentUser } from '../../../store/user/user.selector';
 import { selectIsCartOpen } from '../../../store/cart/cart.selector';
+import { signOutStart } from '../../../store/user/user.action';
 
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
-import { signOutUser } from '../../../utils/firebase/firebase.utils';
-
 import {
   NavigationContainer,
   NavLinks,
@@ -19,8 +18,12 @@ import {
 } from './navigation.styles';
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutStart());
+
   return (
     <Fragment>
       <NavigationContainer>
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -7,6 +7,8 @@ import {
   signInFailed,
   signUpSuccess,
   signUpFailed,
+  signOutSuccess,
+  signOutFailed,
 } from './user.action';
 
 import {
@@ -15,6 +17,7 @@ import {
   signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword,
   createAuthUserWithEmailAndPassword,
+  signOutUser,
 } from '../../utils/firebase/firebase.utils';
 
 export function* gerSnapShotFromUserAuth(userAuth, additionalInfo) {
@@ -73,6 +76,16 @@ export function* signInWithEmail({ payload: { email, password } }) {
   }
 }
 
+//sign-out
+export function* signOut() {
+  try {
+    yield call(signOutUser);
+    yield put(signOutSuccess());
+  } catch (error) {
+    yield put(signOutFailed(error));
+  }
+}
+
 //auth check
 export function* isUserAuthenticated() {
   try {
@@ -104,6 +117,10 @@ export function* onCheckUserSession() {
   yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* onSignOutStart() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
   yield all([
     call(onCheckUserSession),
@@ -111,5 +128,6 @@ export function* userSagas() {
     call(onEmailSignInStart),
     call(onSignUpStart),
     call(onSignUpSuccess),
+    call(onSignOutStart),
   ]);
 }
